a5e4c9663ff33619b8826b49f759dcf51a56ead5
Евгений Златковский <zhenyazl7355608@gmail.com>
2022-12-12 16:13:19 +0100

    my homework

---
+++ homework/hw_3.py
@@ -0,0 +1,23 @@
+#task 1
+print("I love Python " * 42)
+#task 2
+age_in_month = 18 * 12 + 7
+print(age_in_month)
+#task 3
+age_in_years = age_in_month // 12
+print(age_in_years)
+#task 4
+my_age = "My name is Yevhenii, i'm " + str(age_in_years) + " years old."  # or should my name also be inserted through a variable?
+print(my_age)
+#task 5
+brain_cells_left = 1
+brain_cells_left > 2
+brain_cells_left < 0
+brain_cells_left >= 3
+brain_cells_left <= 5
+brain_cells_left != 4
+brain_cells_left == 6
+#task 6
+a, b, c = 2, 5, 6
+d = str(a) + str(b) + str(c)
+print(d)
