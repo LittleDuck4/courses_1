aa82d27b4f3604b6cc27435c02ae9158152f8326
Евгений Златковский <zhenyazl7355608@gmail.com>
2023-02-05 18:31:57 +0100

    requested changes

--- homework/hw_12_and_files/hw_12.py
+++ homework/hw_12_and_files/hw_12.py
@@ -1,19 +1,29 @@
 import json
 from datetime import datetime
 
+
 phone_book = {}
+try:
+    file = open("json")
+except IOError as e:
+    print('failed to open file')
+else:
+    with file:
+        phone_book = json.load(file)
 
 
 class MyCustomException(Exception):
-    pass
+    def __init__(self, message):
+        with open("task2", "a") as f4:
+            print(message, datetime.now(), file=f4)
 
 
 def decor(func):
     def wrap(*args, **kwargs):
-        func(*args, **kwargs)
+        result = func(*args, **kwargs)
         with open("task2", "a") as f4:
             print(f"function: {func.__name__}, time of call: {datetime.now()}", file=f4)
-
+        return result
     return wrap
 
 
@@ -23,8 +33,8 @@ def add():
         print("This number already exists")
     else:
         phone_book[name] = number
-    with open("json", "w") as f2:
-        json.dump(phone_book, f2)
+        with open("json", "w") as f2:
+            json.dump(phone_book, f2)
 
 
 @decor
@@ -37,10 +47,9 @@ def delete():
 
 print("write: \n 'add <name> <number>' to add an entry \n 'delete <name>' to delete the entry by name")
 
+
 while True:
     # try:
-    with open("json") as f1:
-        phone_book = json.load(f1)
     # except Exception as e:
     #     with open("task2", "a") as f4:
     #         print(e, file=f4)
@@ -64,7 +73,5 @@ while True:
     else:
         try:
             zhaba
-        except Exception as e:
-            with open("task2", "a") as f4:
-                print(e, datetime.now(), file=f4)
+        except Exception:
             raise MyCustomException("Custom exception is occured")
